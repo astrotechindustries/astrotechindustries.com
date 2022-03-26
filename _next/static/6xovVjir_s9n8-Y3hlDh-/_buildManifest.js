806e3179c59fc7063f6462534e06f3e59f239e6e
jyunderwood <jyunderwood@users.noreply.github.com>
2022-03-26 18:19:53 +0000

    deploy: 0b63659e1380f27cff758592c7912240cce4e695

--- _next/static/6xovVjir_s9n8-Y3hlDh-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ffa6f7c0b9675dd2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
