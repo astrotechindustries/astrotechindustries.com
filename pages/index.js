c9ae84faec8ad853579fce325d79ae4320fd536b
Jonathan Underwood <jonathan@jyunderwood.com>
2025-01-27 19:03:46 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import ProductCard from "../components/ProductCard"
 
 export default function Home() {
   return (
@@ -14,16 +13,6 @@ export default function Home() {
             <h1 className="text-2xl font-brand text-black dark:text-white">
               Astro Tech Industries
             </h1>
-
-            <p>
-              Web Development Services and iOS Applications
-            </p>
-          </div>
-
-          <div className="space-y-4">
-            <ProductCard comingSoon={true} title={"Astro Merchant"} description={"Space trading adventure game for iOS"} />
-            <ProductCard comingSoon={true} title={"Tic Tac Squared"} description={"A Multiplayer, multidimensional spin on the classic game"} />
-            <ProductCard comingSoon={true} title={"Sight Cards"} description={"Free sight words training app for Pre-K students"} />
           </div>
         </div>
       </main>
